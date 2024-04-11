65286124d714e9905820c07bd038ea6e22b1c217
David Yin <29613693+E137299@users.noreply.github.com>
2024-04-10 22:04:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Caesar Cipher Encrypt/Decrypt
 def caesar(message,shift):
 	pass
+
+#Vigenere Encrypt
+def vigenere_encrypt(text,key):
+	pass
+
+# Vigenere Decrypt
+def vigenere_decrypt(text, key):
+	pass
