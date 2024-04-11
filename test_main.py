6393763029028b64d94702b1c4922083766a93af
David Yin <29613693+E137299@users.noreply.github.com>
2024-04-10 21:06:42 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,3 +4,12 @@ def test_caesar():
   assert caesar("FRIENDSROMANSCOUNTRYMENLENDMEYOUREAR", -20) == "LXOKTJYXUSGTYIUATZXESKTRKTJSKEUAXKGX"
   assert caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ", -3) == "XYZABCDEFGHIJKLMNOPQRSTUVW"
   assert cesar_encrypt("LXOKTJYXUSGTYIUATZXESKTRKTJSKEUAXKGX", 20) == "FRIENDSROMANSCOUNTRYMENLENDMEYOUREAR"
+
+def test_vigenere_encrypt():
+  assert vigenere_encrypt("ATTACKATDAWN", "LEMON") == "LXFOPVEFRNHR"
+  assert vigenere_encrypt("THEEAGLEHASLANDED", "AMERICA") == "TTIVIILETEJTCNDQH"
+
+def test_vignere_decrypt():
+  assert vigenere_decrypt("LXFOPVEFRNHR", "LEMON") == "ATTACKATDAWN"
+  assert vigenere_decrypt("TTIVIILETEJTCNDQH", "AMERICA") == "THEEAGLEHASLANDED"
+  
