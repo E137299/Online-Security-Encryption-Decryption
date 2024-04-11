969c069af7969760c8f66d6fad05043d2ff30b96
David Yin <29613693+E137299@users.noreply.github.com>
2024-04-10 22:03:49 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,15 +1,16 @@
 from main import *
 
 def test_caesar():
-  assert caesar("FRIENDSROMANSCOUNTRYMENLENDMEYOUREAR", -20) == "LXOKTJYXUSGTYIUATZXESKTRKTJSKEUAXKGX"
-  assert caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ", -3) == "XYZABCDEFGHIJKLMNOPQRSTUVW"
-  assert cesar_encrypt("LXOKTJYXUSGTYIUATZXESKTRKTJSKEUAXKGX", 20) == "FRIENDSROMANSCOUNTRYMENLENDMEYOUREAR"
+  assert caesar("FRIENDS ROMANS COUNTRYMEN LEND ME YOUR EAR", -20) == "myplukzgyvthuzgjvau yetlugslukgtlgevayglhy"
+  assert caesar("A quick brown fox jumps over the lazy dog",-23) == "eduymgodfvs rdjsadnyqtwdszivdxlidpecbdhsk"
+  assert caesar("myplukzgyvthuzgjvau yetlugslukgtlgevayglhy", 20) == "friends romans countrymen lend me your ear"
+  assert caesar("eduymgodfvs rdjsadnyqtwdszivdxlidpecbdhsk",23) == "a quick brown fox jumps over the lazy dog"
 
 def test_vigenere_encrypt():
   assert vigenere_encrypt("ATTACK AT DAWN", "LEMON") == "LXEOPVDMGMOEHA"
-  assert vigenere_encrypt("THE EAGLE HAS LANDED.", "AMERICA") == "TTIOMCGLQBYIU LMRUMF"
+  assert vigenere_encrypt("THE EAGLE HAS LANDED", "AMERICA") == "TTIQMCGLQDYIU LMRUMF"
 
 def test_vignere_decrypt():
   assert vigenere_decrypt("LXEOPVDMGMOEHA", "LEMON") == "ATTACK AT DAWN"
-  assert vigenere_decrypt("TTIQMCGLQDYIU LMRUMF", "america") == "THE EAGLE HAS LANDED"
+  assert vigenere_decrypt("TTIQMCGLQDYIU LMRUMF", "AMERICA") == "THE EAGLE HAS LANDED"
   
